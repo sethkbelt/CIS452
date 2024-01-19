8f32bac3b1e312d9d293a3d2b542b57a8e391980
Seth Konynenbelt <sethkbelts@gmail.com>
2024-01-19 09:23:48 -0500

    Added comments

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -22,6 +22,7 @@
 #include <string.h>
 #include <errno.h>
 
+/* Function Prototypes */
 void getUnixInformation();
 void getSystemInformation();
 void getBirthdayInformation();
@@ -34,33 +35,16 @@ int main()
     getBirthdayInformation();
     exit(EXIT_SUCCESS);
 }
-// Use your date of birth as a reference and print your age at the current time in the following format
-// hardcoded birthday of March 20, 2001
-void getBirthdayInformation()
-{
-    struct tm birthday;
-    birthday.tm_year = 101; // tm_year, The number of years since 1900.
-    birthday.tm_mon = 2;
-    birthday.tm_mday = 20;
 
-    time_t nowEpoch = time(NULL);
-    struct tm *nowStruct = gmtime(&nowEpoch);
-    time_t t1 = mktime(nowStruct);
-    time_t t2 = mktime(&birthday);
-    time_t dt = difftime(t1, t2);
-
-    int days = dt / (60 * 60 * 24) - 1;
-    int years = days / 365.25;
-    int months = (days % 365) / 31; // Assuming a month is 30 days
-    days = days - (years * 365.25 + months * 30.437);
-
-    printf("Hardcoded that you were born on March 20, 2001. \n");
-    printf("You are %d years, %d months, and %d days old.\n", years, months, days);
-}
-
+/************************* getUnixInformation() ****************************
+ *  brief : Get and print an assortment of current Unix user and group 
+ *          information
+ *  param : N/A
+ *  return: N/A
+ ***************************************************************************/
 void getUnixInformation()
 {
-    // pointer to current user with system calls
+    // point to current user with system calls
     struct passwd pwd;
     char *me;
     me = getenv("USER");
@@ -77,6 +61,7 @@ void getUnixInformation()
     if (bufsize == -1)   /* Value was indeterminate */
         bufsize = 16384; /* Should be more than enough */
 
+    // indicate a malloc failure
     buf = malloc(bufsize);
     if (buf == NULL)
     {
@@ -84,6 +69,7 @@ void getUnixInformation()
         exit(EXIT_FAILURE);
     }
 
+    // gather necessary Unix information to store in struct s
     s = getpwnam_r(me, &pwd, buf, bufsize, &result);
     if (result == NULL)
     {
@@ -97,6 +83,7 @@ void getUnixInformation()
         exit(EXIT_FAILURE);
     }
 
+    // use the group ID from pwnam to get group information
     struct group *grp;
     grp = getgrgid(pwd.pw_gid);
 
@@ -106,6 +93,7 @@ void getUnixInformation()
     printf("Unix Home Directory     : %s\n", pwd.pw_dir);
     printf("Login Shell             : %s\n", pwd.pw_shell);
     
+    // use lstat from the pwnam to get permissions information
     struct stat sb;
     if (lstat(pwd.pw_dir, &sb) == -1)
     {
@@ -113,25 +101,38 @@ void getUnixInformation()
         exit(EXIT_FAILURE);
     }
 
-    char chmod_buf[100] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+    // custom chmod octal to display converter
+    const int array_size = 100; // big enough for no leak
+    int chmod_size = 10; // sufficient to dislay characters
+    char chmod_buf[array_size] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+    // deciding if input is a directory
     if (S_ISDIR(sb.st_mode) == 1)
         chmod_buf[0] = 'd';
+    
+    // looping through octal representation by 3 to determine permissions
     for (int i = 0; i < 10; i = i + 3)
     {
         if (sb.st_mode & 1 << i)
-            chmod_buf[9 - i] = 'x';
+            chmod_buf[chmod_size - 1 - i] = 'x';
         if (sb.st_mode & 1 << (i + 1))
-            chmod_buf[8 - i] = 'w';
+            chmod_buf[chmod_size - 2 - i] = 'w';
         if (sb.st_mode & 1 << (i + 2))
-            chmod_buf[7 - i] = 'r';
+            chmod_buf[chmod_size - 3 - i] = 'r';
     }
     printf("Home Permission         : %s (%o) \n", chmod_buf, sb.st_mode);
 
+   // free buf pointer
    free(buf);
 }
 
+/************************* getSystemInformation() **************************
+ *  brief : Get and print an assortment of system information
+ *  param : N/A
+ *  return: N/A
+ ***************************************************************************/
 void getSystemInformation()
 {
+    // use utsname struct to get and store system information
     struct utsname unameSys;
     uname(&unameSys);
 
@@ -140,4 +141,35 @@ void getSystemInformation()
 
     printf("Host                    : %s\n", unameSys.nodename);
     printf("System                  : %s %s\n\n", unameSys.sysname, unameSys.release);
+}
+
+/************************* getBirthdayInformation()**************************
+ *  brief : Uses your hard coded date of birth as a reference to print
+ *          your agek at the current time
+ *  param : N/A
+ *  return: N/A
+ ***************************************************************************/
+void getBirthdayInformation()
+{
+    // hardcoding March 20, 2001 birthday
+    struct tm birthday;
+    birthday.tm_year = 101; // tm_year, The number of years since 1900.
+    birthday.tm_mon = 2; //0-11 format
+    birthday.tm_mday = 20;
+
+    // capturing the current time
+    time_t nowEpoch = time(NULL);
+    struct tm *nowStruct = gmtime(&nowEpoch);
+    time_t t1 = mktime(nowStruct);
+    time_t t2 = mktime(&birthday);
+    time_t dt = difftime(t1, t2);
+
+    // math necessary to convert seconds into days, months, years
+    int days = dt / (60 * 60 * 24) - 1;
+    int years = days / 365.25;
+    int months = (days % 365) / 31; // Assuming a month is 31 days (avg is 30.437)
+    days = days - (years * 365.25 + months * 30.437);
+
+    printf("Hardcoded that you were born on March 20, 2001. \n");
+    printf("You are %d years, %d months, and %d days old.\n", years, months, days);
 }
