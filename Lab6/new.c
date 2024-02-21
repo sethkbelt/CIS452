62d594e3f30b37324e891a26cbc35e25224a0dcf
Seth Konynenbelt <sethkbelts@gmail.com>
2024-02-21 00:09:59 -0500

    Some changes on chat room but not much

--- Lab6/new.c
+++ Lab6/new.c
@@ -111,37 +111,21 @@ int main()
     // shmget creates new shared memory and allows user to write to it
 
     if ((shmId =
-
              shmget(my_key, FOO,
-
                     IPC_CREAT | S_IRUSR | S_IWUSR)) < 0)
-
     {
-
         perror("shmget failed to get memory\n");
-
         exit(1);
-
     }
-
-
-
+    printf("shmId %d\b", shmId);
     // attaches the specified shared memory region
-
     // data is the address of the shared memory region
-
     if ((data = shmat(shmId, NULL, 0)) == (void *)-1)
-
     {
-
         perror("Cannot attach to shared memory region\n");
-
         exit(1);
-
     }
-
     printf("data flag\n");
-
     data->flag[0] = 0;
 
     data->flag[1] = 0;
@@ -163,21 +147,15 @@ int main()
     if ((status = pthread_create(&thread1, NULL, writer_function, &data)) != 0)
 
     {
-
         fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
-
         exit(101);
-
     }
 
     if ((status = pthread_create(&thread2, NULL, reader_function, &data)) != 0)
 
     {
-
         fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
-
         exit(101);
-
     }
 
 
@@ -193,35 +171,22 @@ int main()
     if ((status = pthread_join(thread1, &result1)) != 0)
 
     {
-
         fprintf(stderr, "join error %d:%s\n", status, strerror(status));
-
         exit(101);
-
     }
 
     if ((status = pthread_join(thread2, &result2)) != 0)
-
     {
-
         fprintf(stderr, "join error %d: %s\n", status, strerror(status));
-
         exit(101);
-
     }
 
     printf("thread joined up");
-
     // detaches from the shared memory region
-
     if (shmdt(data) < 0)
-
     {
-
         perror("just can't let go\n");
-
         exit(101);
-
     }
 
 
@@ -237,62 +202,30 @@ int main()
 void *reader_function(void *data2)
 
 {
-
     printf("reader \n");
-
     struct shared_data *data = (struct shared_data *)data2;
-
     while (1)
-
     {
-
         data->flag[1] = 1;
-
         data->turn = 0;
-
-        while ( data->flag[0] &&  data->turn == 0)
-
-            ;
-
+        while ( data->flag[0] &&  data->turn == 0);
         printf("%s", data->user_string); // for debugging
-
         data->flag[1] = 0;
-
-    }
-
+   }
     return NULL;
-
 }
-
-
-
 void *writer_function(void *data1)
-
 {   
-
      printf("writer \n");
-
-
-
     struct shared_data *data = (struct shared_data *)data1;
 
     while (1)
-
     {
-
         data->flag[0] = 1;
-
         data->turn = 1;
-
         while ( data->flag[1] &&  data->turn == 1);
-
         fgets(data->user_string, FOO, stdin);
-
-
         data->flag[0] = 0;
-
-    }
-
+}
     return NULL;
-
 }
