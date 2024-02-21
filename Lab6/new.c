9cb2960d93d2c4d4b3a3d0fad5bc87bacbad19a8
Seth Konynenbelt <sethkbelts@gmail.com>
2024-02-21 12:41:43 -0500

    saving off EC

--- Lab6/new.c
+++ Lab6/new.c
@@ -8,108 +8,55 @@
 
  ***************************************************************************/
 
-
-
 /* File include libraries */
 
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <sys/types.h>
-
 #include <sys/stat.h>
-
 #include <sys/ipc.h>
-
 #include <sys/shm.h>
-
 #include <string.h>
-
 #include <pthread.h>
-
 #include <errno.h>
-
 #include <unistd.h>
-
-
+#include <fcntl.h>
 
 /* Pre-defined macros */
-
 #define FOO 4096
 
-
-
 /* Structue defintions */
-
 struct shared_data
-
 {
-
     int flag[2];
-
     int turn;
-
     int quit_flag;
-
     char user_string[4096];
-
 };
 
-
-
 void *do_greeting3(void *arg);
-
 void *writer_function(void *arg);
-
 void *reader_function(void *arg);
 
-
-
 // global (shared and specific) data
-
 int sharedData = 5;
-
 // char val[FOO];
 
-
-
 int main()
-
 {
-
-
-
     // decide which reader you are
 
     // int which_reader = atoi(argv[1]);
 
-
-
     struct shared_data *data;
-
-
-
     int shmId;
-
-
-
     // generate key to the shared memory
-
     key_t my_key = ftok("example.c", 12345);
-
-
-
-    pthread_t thread1, thread2, thread3;
-
-    void *result1, *result2, *result3;
-
+    pthread_t thread1, thread2;
+    void *result1, *result2;
     int status;
 
-
-
     // shmget creates new shared memory and allows user to write to it
-
     if ((shmId =
              shmget(my_key, FOO,
                     IPC_CREAT | S_IRUSR | S_IWUSR)) < 0)
@@ -127,47 +74,29 @@ int main()
     }
     printf("data flag\n");
     data->flag[0] = 0;
-
     data->flag[1] = 0;
-
     data->turn = 0;
-
     data->quit_flag = 12;
-
     // data->user_string = NULL;
-
     printf("post] flag\n");
 
-
-
     // create and start two threads executing the "do_greeting3" function
-
     // pass each thread a pointer to its respective argument
-
     if ((status = pthread_create(&thread1, NULL, writer_function, &data)) != 0)
-
     {
         fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
         exit(101);
     }
 
     if ((status = pthread_create(&thread2, NULL, reader_function, &data)) != 0)
-
     {
         fprintf(stderr, "thread create error %d: %s\n", status, strerror(status));
         exit(101);
     }
 
-
-
     printf("thread create up\n");
-
-
-
     // join with the threads(wait for them to terminate
-
     //  get their return vals
-
     if ((status = pthread_join(thread1, &result1)) != 0)
 
     {
@@ -188,44 +117,38 @@ int main()
         perror("just can't let go\n");
         exit(101);
     }
-
-
-
     return 0;
-
 }
 
-
-
-
-
 void *reader_function(void *data2)
-
 {
     printf("reader \n");
     struct shared_data *data = (struct shared_data *)data2;
     while (1)
     {
-        data->flag[1] = 1;
         data->turn = 0;
-        while ( data->flag[0] &&  data->turn == 0);
-        printf("%s", data->user_string); // for debugging
+        data->flag[1] = 1;
+        while (data->flag[0] && data->turn == 0);
+        printf("%s", data->user_string); // critical section
         data->flag[1] = 0;
-   }
+    }
     return NULL;
 }
 void *writer_function(void *data1)
-{   
-     printf("writer \n");
+{
+    printf("writer \n");
     struct shared_data *data = (struct shared_data *)data1;
+    // Set stdin to non-blocking mode
+    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
 
     while (1)
     {
-        data->flag[0] = 1;
         data->turn = 1;
-        while ( data->flag[1] &&  data->turn == 1);
-        fgets(data->user_string, FOO, stdin);
+        data->flag[0] = 1;
+        while (data->flag[1] && data->turn == 1);
+        fgets(data->user_string, FOO, stdin); // critical section and this is blocking too much
         data->flag[0] = 0;
-}
+    }
     return NULL;
 }
