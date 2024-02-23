2c129a6212b87f85975e94772bbae5e5f4d4026a
Seth Konynenbelt <sethkbelts@gmail.com>
2024-02-23 13:31:44 -0500

    added semaphores to lab7_a

--- Lab7/lab07_a.c
+++ Lab7/lab07_a.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 void* swapper(void*);
 
 int arr[2];
+sem_t mtx;
 
 int main(int argc, char* argv[]) {
     pthread_t who;
     long int loop;
+// Request (called once)
+    sem_init(&mtx,
+  0, /* Process share mode (0:for threads  non-zero:for processes */
+  1  /* initial value */);
 
-    // TODO: get value of loop var (from command line arg)
+    // get value of loop var (from command line arg)
+    loop = atoi(argv[1]);
 
     arr[0] = 0;
     arr[1] = 1;
     pthread_create(&who, NULL, swapper, &loop);
+    int temp;
+    // swap array
     for (int k = 0; k < loop; k++) {
-        // TODO: swap the contents of arr[0] and arr[1]
+        sem_wait(&mtx);
+        temp = arr[0]; // need temp variable to swap
+        arr[0] = arr[1];
+        arr[1] = temp;
+        sem_post(&mtx);
     }
     int rc;
     pthread_join(who, (void **) &rc);
@@ -24,9 +37,15 @@ int main(int argc, char* argv[]) {
 }
 
 void* swapper(void *arg) {
+    int temp;
     long int *num = (long int *) arg;
     for (int k = 0; k < *num; k++) {
-        // TODO: swap the contents of arr[0] and arr[1]
+        sem_wait(&mtx);
+        temp = arr[0]; // need temp variable to swap
+        arr[0] = arr[1];
+        arr[1] = temp;
+        sem_post(&mtx);
+
     }
     return 0;
 }
